b81d23b784796ace1773f65113f13f511df8e7de
SimonGHOSTRiley <41074810+SimonGH0STRiley@users.noreply.github.com>
2021-05-07 10:58:31 +0800

    ✨feat(./demo/index.html ./demo/main.js): 新增平面 还在想平面数据怎么传

--- demo/main.js
+++ demo/main.js
@@ -13,11 +13,14 @@ function main() {
 		attribute vec4 a_position;
 		attribute vec4 a_color;
 		
+		// uniform mat4 u_modelViewMatrix;
 		uniform mat4 u_matrix;
 		
 		varying lowp vec4 v_color;
+		// varying vec3 v_modelViewPosition;
 		
 		void main() {
+			// modelViewePosition = (u_modelViewMatrix * a_position).xyz;
 			gl_Position = u_matrix * a_position;
 			v_color = a_color;
 		}
@@ -27,9 +30,12 @@ function main() {
 	const fsSource = `
 		precision highp float;
 		
+		// uniform mat4 viewMatrix;
+		// uniform mat4 viewNormalMatrix;
 		uniform vec4 u_colorMult;
 		
 		varying lowp vec4 v_color;
+		// varying vec3 v_modelViewPosition;
 		
 		void main() {
 		gl_FragColor = v_color * u_colorMult;
@@ -38,6 +44,7 @@ function main() {
 
 	const programInfo = webglUtils.createProgramInfo(gl, [vsSource, fsSource]);
 
+	const planeBufferInfo		= primitives.createPlaneWithVertexColorsBufferInfo(gl, 20, 20);
 	const topConeBufferInfo		= primitives.createSphereWithVertexColorsBufferInfo(gl, 10, 60, 30)
 	// const bottomConeBufferInfo	= primitives.createTruncatedPyramidWithVertexColorsBufferInfo(gl, 10, 10, 10, 10, 10);
 	const bottomConeBufferInfo	= primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 5, 10, 60, 1, true, true);
@@ -50,26 +57,39 @@ function main() {
 	let fieldOfViewRadians = degToRad(60);
 	let cameraHeight = 50;
 
-	let topConeUniforms = {
-		u_colorMult: [0.5, 1, 0.5, 1],
+	let planeUniforms = {
 		u_matrix: m4.identity(),
+		u_colorMult: [1, 0.5, 0.5, 0.5]
+	};
+	let topConeUniforms = {
+		// u_modelViewMatrix: m4.identity(),
+		u_matrix: m4.identity(),
+		u_colorMult: [0.5, 1, 0.5, 1]
 	};
 	let bottomConeUniforms = {
-		u_colorMult: [0.5, 0.5, 1, 1],
+		// u_modelViewMatrix: m4.identity(),
 		u_matrix: m4.identity(),
+		u_colorMult: [0.5, 0.5, 1, 1]
+
 	};
-	let topConeTranslation 		= [  0, 5, 0];
-	let bottomConeTranslation   = [  0, -5, 0];
+
+	let planeTranslation		= [ 0,  0,  0];
+	let topConeTranslation 		= [ 0,  5,  0];
+	let bottomConeTranslation   = [ 0, -5,  0];
 
 	let objectsToDraw = [{
 		programInfo: programInfo,
+		bufferInfo: planeBufferInfo,
+		uniforms: planeUniforms
+	},{
+		programInfo: programInfo,
 		bufferInfo: topConeBufferInfo,
-		uniforms: topConeUniforms,
+		uniforms: topConeUniforms
 	}, {
 		programInfo: programInfo,
 		bufferInfo: bottomConeBufferInfo,
-		uniforms: bottomConeUniforms,
-	},];
+		uniforms: bottomConeUniforms
+	}];
 
   function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
 	let matrix = m4.translate(viewProjectionMatrix,
@@ -77,7 +97,8 @@ function main() {
 		translation[1],
 		translation[2]);
 	matrix = m4.xRotate(matrix, xRotation);
-	return m4.yRotate(matrix, yRotation);
+	matrix = m4.yRotate(matrix, yRotation);
+	return matrix
   }
 
   requestAnimationFrame(drawScene);
@@ -91,7 +112,7 @@ function main() {
 	// Tell WebGL how to convert from clip space to pixels
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-	gl.enable(gl.CULL_FACE);
+	//gl.enable(gl.CULL_FACE);
 	gl.enable(gl.DEPTH_TEST);
 
 	// Clear the canvas AND the depth buffer.
@@ -99,41 +120,29 @@ function main() {
 
 	// Compute the projection matrix
 	let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-	let projectionMatrix =
-		m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+	let projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
 
 	// Compute the camera's matrix using look at.
 	let cameraPosition = [0, 0, 100];
 	let target = [0, 0, 0];
 	let up = [0, 1, 0];
 	let cameraMatrix = m4.lookAt(cameraPosition, target, up);
-
-	// Make a view matrix from the camera matrix.
 	let viewMatrix = m4.inverse(cameraMatrix);
-
 	let viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
 
+	let planeXRotation	 = -time;
+	let planeYRotation	 =  time;
 	let topConeXRotation =  time;
 	let topConeYRotation =  time;
 	let bottomConeXRotation   =  time;
 	let bottomConeYRotation   = -time;
 
 	// 对每个物体计算矩阵
-	
-	topConeUniforms.u_matrix = computeMatrix(
-		viewProjectionMatrix,
-		topConeTranslation,
-		topConeXRotation,
-		topConeYRotation);
-
-	bottomConeUniforms.u_matrix = computeMatrix(
-		viewProjectionMatrix,
-		bottomConeTranslation,
-		bottomConeXRotation,
-		bottomConeYRotation);
+	planeUniforms.u_matrix = computeMatrix(viewProjectionMatrix, planeTranslation, planeXRotation, planeYRotation);
+	topConeUniforms.u_matrix = computeMatrix(viewProjectionMatrix, topConeTranslation, topConeXRotation, topConeYRotation);
+	bottomConeUniforms.u_matrix = computeMatrix(viewProjectionMatrix, bottomConeTranslation, bottomConeXRotation, bottomConeYRotation);
 
 	// 在这里画物体
-
 	objectsToDraw.forEach(function(object) {
 	  let programInfo = object.programInfo;
 	  let bufferInfo = object.bufferInfo;
