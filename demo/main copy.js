7151e763901d21825a1fffcc608e4ec76abae9af
SimonGHOSTRiley <41074810+SimonGH0STRiley@users.noreply.github.com>
2021-05-25 17:05:23 +0800

    ✨ feat(main copy.js): demo 1.0
    差不多得了 第一版本demo可以发布了 不会有人还会想加功能吧

--- demo/main copy.js
+++ demo/main copy.js
@@ -42,6 +42,7 @@ function main() {
 			float light = abs(dot(normal, u_lightPosition));
 			gl_FragColor = v_color * u_colorMult;
 			gl_FragColor.xyz = gl_FragColor.xyz * light * gl_FragColor.w;
+			
 		}
 	`;
 
@@ -73,6 +74,60 @@ function main() {
 		void main() {
 			gl_FragColor = v_color * u_colorMult;
 			gl_FragColor.a = u_colorMult.a;
+		}
+	`;
+
+	// Clipping vertex shader program
+	const clippingVS = `
+		attribute vec4 a_position;
+		attribute vec4 a_color;
+		
+		uniform mat4 u_modelViewMatrix;
+		uniform mat4 u_modelViewProjectionMatrix;
+		
+		varying lowp vec4 v_color;
+		varying vec3 v_modelViewPosition;
+		
+		void main() {
+			v_modelViewPosition = (u_modelViewMatrix * a_position).xyz;
+			gl_Position = u_modelViewProjectionMatrix * a_position;
+			v_color = a_color;
+		}
+	`;
+
+	// Clipping fragment shader program
+	const clippingFS = `
+		precision highp float;
+		
+		uniform mat4 u_viewMatrix;
+		uniform mat3 u_viewNormalMatrix;
+		uniform vec4 u_clippingPlane;
+		uniform vec4 u_colorMult;
+
+		varying lowp vec4 v_color;
+		varying vec3 v_modelViewPosition;
+
+		vec4 planeToEC(vec4 plane, mat4 viewMatrix, mat3 viewNormalMatrix) {
+			vec3 normal = vec3(plane.x, plane.y, plane.z);
+			vec3 pointInWC = normal * -plane.w;
+			vec3 pointInEC = (viewMatrix * vec4(pointInWC.xyz, 1.0)).xyz;
+			vec3 normalInEC = normalize(viewNormalMatrix * normal);
+			return vec4(normalInEC, -dot(normalInEC, pointInEC));
+		}
+
+		float calDistance(vec4 plane, vec3 position) {
+			float distance = dot(vec3(plane.x, plane.y, plane.z), position) + plane.w;
+			return distance;
+		}
+		
+		void main() {
+			vec4 planeInEC = planeToEC(u_clippingPlane, u_viewMatrix, u_viewNormalMatrix);
+			float distance = calDistance(planeInEC, v_modelViewPosition);
+			float planeSide = dot(v_modelViewPosition, planeInEC.xyz);
+			if (distance * planeSide < 0.0) {
+				discard;
+			}
+			gl_FragColor = v_color * u_colorMult;
 		}
 	`;
 
@@ -122,8 +177,7 @@ function main() {
 		void main() {
 			vec4 planeInEC = planeToEC(u_clippingPlane, u_viewMatrix, u_viewNormalMatrix);
 			float distance = calDistance(planeInEC, v_modelViewPosition);
-			float planeSide = dot(v_modelViewPosition, planeInEC.xyz);
-			if (distance * planeSide < 0.0) {
+			if (distance < 0.0) {
 				discard;
 			}
 			gl_FragColor = v_color * u_colorMult;
@@ -132,6 +186,7 @@ function main() {
 
 	const objectProgram		= webglUtils.createProgramInfo(gl, [objectVS,	objectFS]);
 	const planeProgram		= webglUtils.createProgramInfo(gl, [planeVS,	planeFS]);
+	const clippingProgram	= webglUtils.createProgramInfo(gl, [clippingVS,	clippingFS]);
 	const clippedProgram	= webglUtils.createProgramInfo(gl, [clippedVS,	clippedFS]);
 	
 	const objectBufferInfo	= new Map ([
@@ -145,14 +200,14 @@ function main() {
 		['slinder',		primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 5, 5, 10)],
 		['cone',		primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 0, 5, 10)],
 	]);
+
+	let currentObjectKey = 'default'; 
 	document.getElementById("objectList").addEventListener("change", () => {
 		const objectTypeList = document.getElementsByName("objectType");
 		objectTypeList.forEach((currObject) => {
 			if (currObject.checked) {
-				objectsToDraw[0].bufferInfo = objectBufferInfo.get(currObject.id);
-				objectsToDraw[2].bufferInfo = objectBufferInfo.get(currObject.id);
-				objectsToDraw[3].bufferInfo = objectBufferInfo.get(currObject.id);
-				objectsToDraw[4].bufferInfo = objectBufferInfo.get(currObject.id);
+				currentObjectKey = currObject.id;
+				changeObjectsMap(objectsMapToDraw, objectBufferInfo.get(currentObjectKey));
 			}
 		});
 	});
@@ -183,7 +238,7 @@ function main() {
 		document.getElementById("objectList").style.display = "none";
 		document.getElementById("sliderList").style.display = "none";
 
-		const frontObjBufferInfo	= objectsToDraw[0].bufferInfo;
+		const frontObjBufferInfo	= objectBufferInfo.get(currentObjectKey);
 		const backObjBufferInfo		= frontObjBufferInfo;
 		frontObjBufferInfo.useStencil		= backObjBufferInfo.useStencil		= true;
 		frontObjBufferInfo.stencilClear		= backObjBufferInfo.stencilClear	= true;
@@ -201,24 +256,7 @@ function main() {
 
 		clippingFront	= m4.transformVector(m4.inverse(m4.transpose(clippingTransformMatrix)), m4.createVec4FromValues(0, 1, 0, 0));
 		clippingBack	= m4.reverseVec4(clippingFront);
-		objectsToDraw = [{
-			programInfo: clippedProgram,
-			bufferInfo: frontObjBufferInfo,
-			uniforms: frontObjectUniforms
-		}, {
-			programInfo: planeProgram,
-			bufferInfo: clippingFrontBufferInfo,
-			uniforms: frontPlaneUniforms
-		}, {
-			programInfo: clippedProgram,
-			bufferInfo: backObjBufferInfo,
-			uniforms: backObjectUniforms
-		}, {
-			programInfo: planeProgram,
-			bufferInfo: clippingBackBufferInfo,
-			uniforms: backPlaneUniforms
-		}
-		];
+		clippingObjectsMap(objectsMapToDraw, frontObjBufferInfo, backObjBufferInfo, clippingFrontBufferInfo, clippingBackBufferInfo);
 	});
 	document.getElementById("reset").addEventListener("click", () => {
 		// location.reload();
@@ -226,17 +264,7 @@ function main() {
 		document.getElementById("objectList").style.display = "block";
 		document.getElementById("sliderList").style.display = "block";
 
-		objectsToDraw.splice(2, 2);
-		objectsToDraw = [{
-			programInfo: objectProgram,
-			bufferInfo: objectsToDraw[0].bufferInfo,
-			uniforms: objectUniforms
-		}, {
-			programInfo: planeProgram,
-			bufferInfo: planeBufferInfo,
-			uniforms: planeUniforms
-		}
-		];
+		initObjectsMap(objectsMapToDraw);
 	});
 
 	function degToRad(d) {
@@ -247,17 +275,15 @@ function main() {
 	let fieldOfViewRadians = degToRad(60);
 	let cameraHeight = 50;
 
-	const lightPosition = m4.normalize([20, 20, 50]);
+	const lightPosition = m4.normalize([20, 50, 50]);
 	let isClipped = false;
 
 	let objectUniforms = {
 		u_modelViewProjectionMatrix: null,
 		u_colorMult: [0.5, 1, 0.5, 0.8],
-		//u_colorMult: [0.5, 1, 0.5, 0.8],
 		u_normalMatrix: null,
 		u_lightPosition: null
 	};
-
 	let objectClippedUniforms = {
 		u_modelMatrix: null,
 		u_viewMatrix: null,
@@ -267,17 +293,14 @@ function main() {
 		u_clippingPlane: null,
 		u_colorMult: [0.5, 0.5, 1, 1]
 	};
-
 	let planeUniforms = {
 		u_modelViewProjectionMatrix: null,
 		u_colorMult: [0, 0, 1, 0.5]
 	};
-
 	let planeInnerUniforms = {
 		u_modelViewProjectionMatrix: null,
 		u_colorMult: [0, 0, 1, 0.8]
 	};
-
 	let frontObjectUniforms = {
 		u_modelMatrix: null,
 		u_viewMatrix: null,
@@ -309,97 +332,183 @@ function main() {
 	let frontObjectTranslation 		= [  0,  7,  0];
 	let backObjectTranslation 		= [  0, -7,  0];
 
-	// let objectsToDraw = new Map (
-	// 	['draw', {
-	// 		// 画几何体背面到深度缓冲
-	// 		programInfo: objectProgram,
-	// 		bufferInfo: objectBufferInfo.get('default'),
-	// 		uniforms: objectUniforms,
-	// 		opt: 1,
-	// 		renderOption: {
-	// 			disableColor: true,
-	// 			cullFace: gl.FRONT,
-	// 		},
-	// 	}],
+	let objectsMapToDraw = new Map ();
+	initObjectsMap(objectsMapToDraw);
 
-	// );
-	let  objectsToDraw = [{
-			// 画几何体背面到深度缓冲
-			programInfo: objectProgram,
-			bufferInfo: objectBufferInfo.get('default'),
-			uniforms: objectUniforms,
-			opt: 1,
-			renderOption: {
-				disableColor: true,
-				cullFace: gl.FRONT,
+	function initObjectsMap(objectsMap) {
+		objectsMap.clear();
+		objectsMap.set(
+			'fillDepthBuffer', {
+				// 填充几何体背面到深度缓冲
+				programInfo: objectProgram,
+				bufferInfo: objectBufferInfo.get('default'),
+				uniforms: objectUniforms,
+				renderOption: {
+					disableColor: true,
+					cullFace: gl.FRONT,
+				}
 			}
-		}, {
-			// 画在几何体后面的平面
-			programInfo: planeProgram,
-			bufferInfo: planeBufferInfo,
-			uniforms: planeUniforms,
-			step: 2,
-			renderOption: {
-				disableColor: false,
-				disableDepthWrite: true,
-				depthFunc: gl.GREATER,
+		).set(
+			'drawBackPlane', {
+				// 画在几何体后的平面
+				programInfo: planeProgram,
+				bufferInfo: planeBufferInfo,
+				uniforms: planeUniforms,
+				renderOption: {
+					disableColor: false,
+					disableDepthWrite: true,
+					depthFunc: gl.GREATER,
+				}
 			}
-		}, {
-			// 3
-			programInfo: objectProgram,
-			bufferInfo: objectBufferInfo.get('default'),
-			uniforms: objectUniforms,
-			renderOption: {
-				disableDepth: true,
-				depthFunc: gl.LESS,
-				cullFace: gl.FRONT,
-			},
-		}, {
-			// 4
-			programInfo: clippedProgram,
-			bufferInfo: objectBufferInfo.get('default'),
-			uniforms: objectClippedUniforms,
-			renderOption: {
-				clearDepth: true,
-				useStencil: true,
-				stencilWrite: true,
-				disableColor: true,
-				stencilBackOp: [gl.KEEP, gl.KEEP, gl.INCR],
-				stencilFrontOp: [gl.KEEP, gl.KEEP, gl.DECR],
-				stencilFunc: [gl.ALWAYS, 1, 0xFF],
+		).set(
+			'drawBackObject', {
+				// 画几何体背面
+				programInfo: objectProgram,
+				bufferInfo: objectBufferInfo.get('default'),
+				uniforms: objectUniforms,
+				renderOption: {
+					disableDepth: true,
+					depthFunc: gl.LESS,
+					cullFace: gl.FRONT,
+				}
 			}
-		}, {
-		// 	// 6
-			programInfo: objectProgram,
-			bufferInfo: objectBufferInfo.get('default'),
-			uniforms: objectUniforms,
-			renderOption: {
-				clearDepth: true,
-				cullFace: gl.BACK,
-			},
-		}, {
-			// 7
-			programInfo: planeProgram,
-			bufferInfo: planeBufferInfo,
-			uniforms: planeUniforms,
-			renderOption: {
-				useStencil: true,
-				stencilOp: [gl.KEEP, gl.KEEP, gl.KEEP],
-				stencilFunc: [gl.NOTEQUAL, 1, 0xFF],
+		).set(
+			'fillModelBuffer', {
+				// 填充切面背后的几何体到模版缓冲
+				programInfo: clippingProgram,
+				bufferInfo: objectBufferInfo.get('default'),
+				uniforms: objectClippedUniforms,
+				renderOption: {
+					clearDepth: true,
+					useStencil: true,
+					stencilWrite: true,
+					disableColor: true,
+					stencilBackOp: [gl.KEEP, gl.KEEP, gl.INCR],
+					stencilFrontOp: [gl.KEEP, gl.KEEP, gl.DECR],
+					stencilFunc: [gl.ALWAYS, 1, 0xFF],
+				}
 			}
-		}, {
-			// Draw clipping plane
-			programInfo: planeProgram,
-			bufferInfo: planeBufferInfo,
-			uniforms: planeInnerUniforms,
-			renderOption: {
-				disableDepth: true,
-				useStencil: true,
-				stencilOp: [gl.KEEP, gl.KEEP, gl.KEEP],
-				stencilFunc: [gl.EQUAL, 1, 0xFF],
+		).set(
+			'drawFrontObject', {
+				// 画几何体正面
+				programInfo: objectProgram,
+				bufferInfo: objectBufferInfo.get('default'),
+				uniforms: objectUniforms,
+				renderOption: {
+					clearDepth: true,
+					cullFace: gl.BACK,
+				}
 			}
-		}
-	];
+		).set(
+			'drawFrontPlane', {
+				// 画在几何体前的平面
+				programInfo: planeProgram,
+				bufferInfo: planeBufferInfo,
+				uniforms: planeUniforms,
+				renderOption: {
+					useStencil: true,
+					stencilOp: [gl.KEEP, gl.KEEP, gl.KEEP],
+					stencilFunc: [gl.NOTEQUAL, 1, 0xFF],
+				}
+			}
+		).set(
+			'drawClippingPlane', {
+				// 画切面
+				programInfo: planeProgram,
+				bufferInfo: planeBufferInfo,
+				uniforms: planeInnerUniforms,
+				renderOption: {
+					disableDepth: true,
+					useStencil: true,
+					stencilOp: [gl.KEEP, gl.KEEP, gl.KEEP],
+					stencilFunc: [gl.EQUAL, 1, 0xFF],
+				}
+			}
+		);
+	}
+	
+	function changeObjectsMap(objectsMap, objectBuffer) {
+		objectsMap.set(
+			'fillDepthBuffer', {
+				// 填充几何体背面到深度缓冲
+				programInfo: objectProgram,
+				bufferInfo: objectBuffer,
+				uniforms: objectUniforms,
+				renderOption: {
+					disableColor: true,
+					cullFace: gl.FRONT,
+				}
+			}
+		).set(
+			'drawBackObject', {
+				// 画几何体背面
+				programInfo: objectProgram,
+				bufferInfo: objectBuffer,
+				uniforms: objectUniforms,
+				renderOption: {
+					disableDepth: true,
+					depthFunc: gl.LESS,
+					cullFace: gl.FRONT,
+				}
+			}
+		).set(
+			'fillModelBuffer', {
+				// 填充切面背后的几何体到模版缓冲
+				programInfo: clippingProgram,
+				bufferInfo: objectBuffer,
+				uniforms: objectClippedUniforms,
+				renderOption: {
+					clearDepth: true,
+					useStencil: true,
+					stencilWrite: true,
+					disableColor: true,
+					stencilBackOp: [gl.KEEP, gl.KEEP, gl.INCR],
+					stencilFrontOp: [gl.KEEP, gl.KEEP, gl.DECR],
+					stencilFunc: [gl.ALWAYS, 1, 0xFF],
+				}
+			}
+		).set(
+			'drawFrontObject', {
+				// 画几何体正面
+				programInfo: objectProgram,
+				bufferInfo: objectBuffer,
+				uniforms: objectUniforms,
+				renderOption: {
+					clearDepth: true,
+					cullFace: gl.BACK,
+				}
+			}
+		);
+	}
+
+	function clippingObjectsMap(objectsMap, frontObjBufferInfo, backObjBufferInfo, clippingFrontBufferInfo, clippingBackBufferInfo) {
+		objectsMap.clear();
+		objectsMap.set(
+			'frontObj', {
+				programInfo: clippedProgram,
+				bufferInfo: frontObjBufferInfo,
+				uniforms: frontObjectUniforms
+			}
+		).set(
+			'frontPlane', {
+				programInfo: planeProgram,
+				bufferInfo: clippingFrontBufferInfo,
+				uniforms: frontPlaneUniforms
+			}
+		).set(
+			'backObj', {
+				programInfo: clippedProgram,
+				bufferInfo: backObjBufferInfo,
+				uniforms: backObjectUniforms
+			}
+		).set(
+			'backPlane', {
+				programInfo: planeProgram,
+				bufferInfo: clippingBackBufferInfo,
+				uniforms: backPlaneUniforms
+			}
+		);
+	}
 
 	function computeObjectMatrix(viewProjectionMatrix, translation, rotation) {
 		let matrix = m4.translate(viewProjectionMatrix,
@@ -535,108 +644,123 @@ function main() {
 		
 		
 		// 在这里画物体
-		objectsToDraw.forEach(function(object) {
-		const programInfo = object.programInfo;
-		const bufferInfo = object.bufferInfo;
-		const renderOption = object.renderOption || {};
-		let bindBuffers = false;
+		objectsMapToDraw.forEach(function(object) {
+			const programInfo = object.programInfo;
+			const bufferInfo = object.bufferInfo;
+			const renderOption = object.renderOption || {};
+			let bindBuffers = false;
 
-		if (programInfo !== lastUsedProgramInfo) {
-			lastUsedProgramInfo = programInfo;
-			gl.useProgram(programInfo.program);
-			// 更换程序后要重新绑定缓冲，因为只需要绑定程序要用的缓冲。
-    		// 如果两个程序使用相同的bufferInfo但是第一个只用位置数据，
-    		// 当我们从第一个程序切换到第二个时，有些属性就不存在。
-    		bindBuffers = true;
-		}
-
-		// 设置属性
-		if (bindBuffers || bufferInfo !== lastUsedBufferInfo) {
-			lastUsedBufferInfo = bufferInfo;
-			webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-		}
-
-		// 设置uniform变量
-		webglUtils.setUniforms(programInfo, object.uniforms);
-		/*
-		if (!isClipped) {
-			if (programInfo === objectProgram) {
-				gl.depthMask(true);
-				//gl.disable(gl.BLEND);
-				gl.enable(gl.BLEND);
-				gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-			} else if (programInfo === planeProgram) {
-				gl.depthMask(false);
-				gl.enable(gl.BLEND);
-				gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+			if (programInfo !== lastUsedProgramInfo) {
+				lastUsedProgramInfo = programInfo;
+				gl.useProgram(programInfo.program);
+				// 更换程序后要重新绑定缓冲，因为只需要绑定程序要用的缓冲。
+				// 如果两个程序使用相同的bufferInfo但是第一个只用位置数据，
+				// 当我们从第一个程序切换到第二个时，有些属性就不存在。
+				bindBuffers = true;
 			}
-		} else {
-			gl.depthMask(true);
-			//gl.disable(gl.BLEND);
-		}
-		*/
-		if (renderOption.disableDepth) {
-			gl.disable(gl.DEPTH_TEST);
-		} else {
-			gl.enable(gl.DEPTH_TEST);
-		}
-		if (renderOption.clearDepth) {
-			gl.depthMask(true);
-			gl.clear(gl.DEPTH_BUFFER_BIT);
-		}
-		if (renderOption.disableDepthWrite) {
-			gl.depthMask(false);
-		} else {
-			gl.depthMask(true);
-		}
-		if (renderOption.depthFunc) {
-			gl.depthFunc(renderOption.depthFunc);
-		} else {
-			gl.depthMask(gl.LESS);
-		}
 
-		if (renderOption.disableColor) {
-			gl.colorMask(false, false, false, false);
-		} else {
-			gl.colorMask(true, true, true, true);
-		}
+			// 设置属性
+			if (bindBuffers || bufferInfo !== lastUsedBufferInfo) {
+				lastUsedBufferInfo = bufferInfo;
+				webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+			}
 
-		if (renderOption.cullFace) {
-			gl.enable(gl.CULL_FACE);
-			gl.cullFace(renderOption.cullFace);
-		} else {
-			gl.disable(gl.CULL_FACE);
-		}
+			// 设置uniform变量
+			webglUtils.setUniforms(programInfo, object.uniforms);
+			
+			if (!isClipped) {
+				if (renderOption.disableDepth) {
+					gl.disable(gl.DEPTH_TEST);
+				} else {
+					gl.enable(gl.DEPTH_TEST);
+				}
+				if (renderOption.clearDepth) {
+					gl.depthMask(true);
+					gl.clear(gl.DEPTH_BUFFER_BIT);
+				}
+				if (renderOption.disableDepthWrite) {
+					gl.depthMask(false);
+				} else {
+					gl.depthMask(true);
+				}
+				if (renderOption.depthFunc) {
+					gl.depthFunc(renderOption.depthFunc);
+				} else {
+					gl.depthMask(gl.LESS);
+				}
 
-		// Stencil 相关
-		if (renderOption.useStencil) {
-			gl.enable(gl.STENCIL_TEST);
-		} else {
-			gl.disable(gl.STENCIL_TEST);
-		}
-		if (renderOption.stencilClear) {
-			gl.stencilMask(0xFF);
-			gl.clear(gl.STENCIL_BUFFER_BIT);
-		}
-		if (renderOption.stencilWrite) {
-			gl.stencilMask(0xFF);
-		} else {
-			gl.stencilMask(0);
-		}
-		if (renderOption.stencilOp) {
-			gl.stencilOp(renderOption.stencilOp[0], renderOption.stencilOp[1], renderOption.stencilOp[2]);
-		}
-		if (renderOption.stencilFrontOp) {
-			gl.stencilOpSeparate(gl.FRONT, renderOption.stencilFrontOp[0], renderOption.stencilFrontOp[1], renderOption.stencilFrontOp[2]);
-		}
-		if (renderOption.stencilBackOp) {
-			gl.stencilOpSeparate(gl.BACK, renderOption.stencilBackOp[0], renderOption.stencilBackOp[1], renderOption.stencilBackOp[2]);
-		}
-		if (renderOption.stencilFunc) {
-			gl.stencilFunc(renderOption.stencilFunc[0], renderOption.stencilFunc[1], renderOption.stencilFunc[2])
-		}
-		// 绘制3D图形
-		gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
+				if (renderOption.disableColor) {
+					gl.colorMask(false, false, false, false);
+				} else {
+					gl.colorMask(true, true, true, true);
+				}
+
+				if (renderOption.cullFace) {
+					gl.enable(gl.CULL_FACE);
+					gl.cullFace(renderOption.cullFace);
+				} else {
+					gl.disable(gl.CULL_FACE);
+				}
+
+				// Stencil 相关
+				if (renderOption.useStencil) {
+					gl.enable(gl.STENCIL_TEST);
+				} else {
+					gl.disable(gl.STENCIL_TEST);
+				}
+				if (renderOption.stencilClear) {
+					gl.stencilMask(0xFF);
+					gl.clear(gl.STENCIL_BUFFER_BIT);
+				}
+				if (renderOption.stencilWrite) {
+					gl.stencilMask(0xFF);
+				} else {
+					gl.stencilMask(0);
+				}
+				if (renderOption.stencilOp) {
+					gl.stencilOp(renderOption.stencilOp[0], renderOption.stencilOp[1], renderOption.stencilOp[2]);
+				}
+				if (renderOption.stencilFrontOp) {
+					gl.stencilOpSeparate(gl.FRONT, renderOption.stencilFrontOp[0], renderOption.stencilFrontOp[1], renderOption.stencilFrontOp[2]);
+				}
+				if (renderOption.stencilBackOp) {
+					gl.stencilOpSeparate(gl.BACK, renderOption.stencilBackOp[0], renderOption.stencilBackOp[1], renderOption.stencilBackOp[2]);
+				}
+				if (renderOption.stencilFunc) {
+					gl.stencilFunc(renderOption.stencilFunc[0], renderOption.stencilFunc[1], renderOption.stencilFunc[2])
+				}
+			} else {
+				gl.depthMask(true);
+				gl.disable(gl.BLEND);
+				if (bufferInfo.useStencil) {
+					gl.enable(gl.STENCIL_TEST);
+				} else {
+					gl.disable(gl.STENCIL_TEST);
+				}
+				if (bufferInfo.stencilClear) {
+					gl.stencilMask(0xFF);
+					gl.clear(gl.STENCIL_BUFFER_BIT);
+				}
+				if (bufferInfo.stencilWrite) {
+					gl.stencilMask(0xFF);
+				} else {
+					gl.stencilMask(0);
+				}
+				if (bufferInfo.stencilOp) {
+					gl.stencilOp(bufferInfo.stencilOp[0], bufferInfo.stencilOp[1], bufferInfo.stencilOp[2]);
+				}
+				if (bufferInfo.stencilFrontOp) {
+					gl.stencilOpSeparate(gl.FRONT, bufferInfo.stencilFrontOp[0], bufferInfo.stencilFrontOp[1], bufferInfo.stencilFrontOp[2]);
+				}
+				if (bufferInfo.stencilBackOp) {
+					gl.stencilOpSeparate(gl.BACK, bufferInfo.stencilBackOp[0], bufferInfo.stencilBackOp[1], bufferInfo.stencilBackOp[2]);
+				}
+				if (bufferInfo.stencilFunc) {
+					gl.stencilFunc(bufferInfo.stencilFunc[0], bufferInfo.stencilFunc[1], bufferInfo.stencilFunc[2])
+				}
+			}
+			// 绘制3D图形
+			gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
 		});
 
 		requestAnimationFrame(drawScene);
